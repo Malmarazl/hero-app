61835f4def1a2bb9edfbf41dee8a379af340c1d4
Monica Almaraz <malmaraz@hiberus.com>
2022-07-08 10:03:45 +0200

    fix routing, remove unnecesary packages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'heroes', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'edit/:id', component: EditHeroComponent }
